cce47c4c7847d0385c4d166537db63a35d6d68e1
PhuocLoc <locdtp0807@gmail.com>
2023-01-29 00:18:09 +0700

    Initial Commit

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by Phước Lộc Đinh Tạ on 29/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
